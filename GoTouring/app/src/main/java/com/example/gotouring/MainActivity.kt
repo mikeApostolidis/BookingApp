1ad54d6f674de24aecbde323040346a32d19a2d1
ESCANORssama-tech <60557423+ESCANORssama-tech@users.noreply.github.com>
2022-04-26 11:45:11 +0300

    Room api depedency

--- GoTouring/app/src/main/java/com/example/gotouring/MainActivity.kt
+++ GoTouring/app/src/main/java/com/example/gotouring/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.gotouring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
