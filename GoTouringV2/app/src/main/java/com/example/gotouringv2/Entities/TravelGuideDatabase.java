2cc2a39b5ab665a5be971b6d0e3088302ba197af
mikeApostolidis <mike23apostolidis@gmail.com>
2022-05-18 13:54:01 +0300

    Revert "Room api"
    This reverts commit 8274a4a39d8486074d8d6fb3d5e8d6fcc6a4efe4.

--- GoTouringV2/app/src/main/java/com/example/gotouringv2/Entities/TravelGuideDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.gotouringv2.Entities;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {TravelAgency.class,TripInfo.class,TravelPackage.class},version = 1)
-public abstract class TravelGuideDatabase extends RoomDatabase {
-    public abstract TravelGuideDao travelGuideDao();
-}
